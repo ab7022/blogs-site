576312c9a6a07a0243cb847d67bd0650ef3facfc
abdul bayees <bayees1@gmail.com>
2023-08-13 20:00:32 +0530

    added new functions

--- routes/blog.js
+++ routes/blog.js
@@ -1,40 +1,61 @@
 const express = require("express")
+
 const db = require("../data/database")
 const router = express.Router()
+router.use(express.urlencoded({ extended: true }));
 router.get("/",function(req,res){
 res.redirect("/posts")
 })
 
-router.get("/posts",function(req,res){
-res.render("posts-list")
+router.get("/posts", async function(req,res){
+  const query = `SELECT posts.*, authors.name AS author_name FROM posts
+  INNER JOIN authors ON posts.author_id = authors.id `
+ const [posts] = await db.query(query)
+res.render("posts-list",{posts:posts})
 })
+
+router.get("/posts/:id", async function(req,res){
+  const query =  `SELECT posts.*, authors.name AS author_name,authors.email AS author_email FROM posts 
+  INNER JOIN authors ON posts.author_id =authors.id
+  WHERE posts.id = ?`
+  const [posts] = await db.query(query,[req.params.id])
+  if(!posts || posts.length===0){
+    return res.status(404).render("404")
+  }
+  const postData ={
+    ...posts[0],
+    date:posts[0].date.toISOString(),
+    humanReadableDate:posts[0].date.toLocaleDateString("en-US",{
+      weekday:"long",
+      year:"numeric",
+      month:"long",
+      day:"numeric"
+    })
+  }
+  res.render("post-detail",{post:postData})
+})
+
+router.get("/posts/:id/edit", async function(req,res){
+  res.render("update-post")
+})
+
 router.get("/new-post",async function(req,res){
   const [authors] = await db.query("SELECT *FROM authors")
 res.render("create-post",{authors:authors})
 })
 
-// router.post("/posts", async function(req,res){
-//     const data = [
-//      req.body.title,
-//      req.body.summary,
-//  req.body.content,
-//  req.body.author,
-//     ]    
-//    await db.query("INSERT INTO posts (title,summary,body,author_id) VALUES(?)",[data[0]])
-//     res.redirect("/posts")
-// })
+
+
+
 router.post("/posts", async function(req, res) {
+  console.log(req.body)
     const data = [
         req.body.title,
         req.body.summary,
         req.body.content,
         req.body.author
     ];
-    // Add this line before the db.query line
-
-    // Use question marks as placeholders for values and provide all values from the data array
-    await db.query("INSERT INTO posts (title, summary, body, author_id) VALUES (?)", [data,]);
-
+    await db.query("INSERT INTO posts(title, summary, body, author_id) VALUES (?,?,?,?)", [data[0],data[1],data[2],data[3]]);
     res.redirect("/posts");
 });
 
